5c6dd31f1fe4ae9b3af9fc8f6ef9460b5e0236bd
Jessie Liu <hanyan.liu@outlook.com>
2021-10-22 20:47:50 +1100

    lesson19 useEffect error handling

--- src/home.js
+++ src/home.js
@@ -2,38 +2,46 @@ import { useEffect, useState } from "react";
 import BlogList from "./blogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(undefined); // or null
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
 
-  const [name, setName] = useState("mario");
-
   useEffect(() => {
-    console.log("useEffect ran");
-  }, [name]);
+    //this setTimeout is only to stimulate the delayed loading effect, not necessary here!
+    setTimeout(() => {
+      fetch("http://localhost:8000/blog")
+        .then((res) => {
+          if (!res.ok) {
+            // this must be put before the throw error msg to be executed.
+            setIsPending(false);
+            throw Error("cann't fetch the source");
+          }
+
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        })
+        .catch((err) => {
+          // console.log(err);
+          setError(err.message);
+        });
+    }, 1000);
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="All blogs"
-        handleDelete={handleDelete}
-      />
-      <button onClick={() => setName("Jessie")}>change name</button>
-      <p>{name}</p>
+      {isPending && <p>Loading...</p>}
+      {<div> {error}</div>}
+      {blogs && (
+        <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      )}
     </div>
   );
 };
